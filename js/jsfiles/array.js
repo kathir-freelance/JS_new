cb9891f07ca3bce543fdbd31d0980a093994fdbc
kathir-cf[gui] <kathir_a1@outlook.com>
2022-09-26 20:54:30 +0530

    updated

--- js/jsfiles/array.js
+++ js/jsfiles/array.js
@@ -1,5 +1,5 @@
 var varName=[];//declcration
-varName=[1,2,43,56,6,7,88];
+varName=[1,2,43,56,6,7,88];//initialization
 // console.log(varName[0]);//1
 // console.log(varName[2]);//43
 // console.log(varName[4]);//6
@@ -12,7 +12,21 @@ varName=[1,2,43,56,6,7,88];
 //for(var i=0;i<8;i+=2)
   //  console.log(varName[i]);
 
-console.log(varName.length)
+//console.log(varName.length)
 
-for(var i=0;i<varName.length;i++)
-    console.log(varName[i]);
+//for(var i=0;i<varName.length;i++)
+    //console.log(varName[i]);
+
+
+var arr=[3,4,45,5,6,6,7,3,8,81];
+var d=90; var e=89; var f=34;
+var arr2=Array.of(d,e,f);    
+console.log(arr2);
+
+var str1="hi hello how";
+var str2=Array.from(str1);
+console.log(str2)
+
+var s1="hi";var s2="hello"; var s3="how";
+var str3=Array.of(s1,s2,s3);
+console.log(str3)
