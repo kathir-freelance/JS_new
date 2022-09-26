c49b83916c3877a99b1daaa6178c18886d510c8d
kathir-cf[gui] <kathir_a1@outlook.com>
2022-09-26 20:45:03 +0530

    arrays

---
+++ js/jsfiles/array.js
@@ -0,0 +1,18 @@
+var varName=[];//declcration
+varName=[1,2,43,56,6,7,88];
+// console.log(varName[0]);//1
+// console.log(varName[2]);//43
+// console.log(varName[4]);//6
+// console.log(varName[7]);//undefined
+// console.log(varName[8]);//undefined
+
+//for(var i=0;i<6;i++)
+ //   console.log(varName[i])
+
+//for(var i=0;i<8;i+=2)
+  //  console.log(varName[i]);
+
+console.log(varName.length)
+
+for(var i=0;i<varName.length;i++)
+    console.log(varName[i]);
