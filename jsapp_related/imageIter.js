66f503db67d48c71a3d2546b83861c17cca7e386
kathir-cf[gui] <kathir_a1@outlook.com>
2023-01-08 13:05:19 +0530

    save

--- jsapp_related/imageIter.js
+++ jsapp_related/imageIter.js
@@ -26,3 +26,35 @@ console.log('in print',all)
 }
 
 // call()
+function call1(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+     return response.json();
+    })
+    .then(data => 
+        {
+            all=data;
+            console.log(all)
+            print1(all)
+          //  document.write(`<img src=${all[0].image} alt='no image'/>`)
+        }
+        );
+    }
+    call1()
+function print1(all)
+{
+console.log('in print',all)
+    for(let ob of all)
+    {
+        // document.write('hi')
+        // document.write(`<input value=${ob.username} readonly/>`)
+        // document.write(`<div>${ob.id}</div>`)
+        // document.write(`<div>${ob.name}</div>`)
+        var newList=document.createElement('select')
+        newList.appendChild(new Option("my label 1", "my value 1"));
+        newList.appendChild(new Option("my label 2", "my value 2"));
+        newList.appendChild(new Option("my label 3", "my value 3"));
+       // sel.appendChild(op)
+        document.getElementById('content').appendChild(newList)
+    }
+}
